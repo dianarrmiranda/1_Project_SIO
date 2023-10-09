9b5da6ad5ef4475c2c72475a5f3057d63a478355
Dan1M4d <daniel.madureira@ua.pt>
2023-10-09 19:09:45 +0100

    Added navbar and go back button to product page

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
   }, [theme]);
 
   return (
-    <div className=" bg-secondary w-full flex justify-between items-center p-2 ">
+    <div className="navbar bg-secondary w-full flex justify-between items-center p-2">
       <Link
         to="/"
         className="max-w-[5%]"
--- frontend/src/components/pages/ProductPage.jsx
+++ frontend/src/components/pages/ProductPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import Navbar from '../layout/Navbar'
 
 const ProductPage = () => {
     const {id} = useParams()
+    const navigate = useNavigate()
   return (
     <div>
+      <Navbar />
+      <button className='btn btn-accent' onClick={() => navigate(-1)}>GO BACK</button>
         <h1>Product {id}</h1>
     </div>
   )
