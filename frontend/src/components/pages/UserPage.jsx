a1e5590e0edc9e3de7a09f339f7d3fff227de543
dianarrmiranda <dianarrmiranda@ua.pt>
2023-10-20 12:25:14 +0100

    Starting doing method of changing password

--- frontend/src/components/pages/UserPage.jsx
+++ frontend/src/components/pages/UserPage.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import Navbar from '../layout/Navbar';
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Navbar from "../layout/Navbar";
 
-import { fetchData } from '../../utils';
-import Footer from '../layout/Footer';
+import { fetchData } from "../../utils";
+import Footer from "../layout/Footer";
 
 const ProductPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
   const [user, setUser] = useState({});
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
   useEffect(() => {
     const initialize = async () => {
@@ -20,30 +22,97 @@ const ProductPage = () => {
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('user');
-    navigate('/login');
+    localStorage.removeItem("user");
+    navigate("/login");
   };
 
-  console.log('User ->', user);
+  const handleChangePass = () => {
+    if (password === newPassword) {
+      const initialize = async () => {
+        const data = await fetchData(`/user/changePass?id=${id}&pass=${password}`);
+        setUser(data);
+      };
+      initialize();
+    }
+  };
+
+
+  console.log("User ->", user);
 
   return (
     <div>
       <Navbar />
-      
+
       <div className="flex flex-wrap">
-        <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4">
-          <img src={"../../"+user.image} alt="User Image" className="w-full h-full object-cover" />
-        </div>
         <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4">
           <h1 className="text-2xl font-bold mb-2">{user.name}</h1>
           <p className="text-lg mb-2">{user.email}</p>
-          <button className="btn btn-accent mb-2" onClick={handleLogout}>
-            Logout
-          </button>
+
+          <div className="flex">
+            <button
+              className="btn btn-accent mb-2 mr-2"
+              onClick={() =>
+                document.getElementById("modal_ChangePass").showModal()
+              }
+            >
+              Change Password
+            </button>
+            <button className="btn btn-accent mb-2" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        </div>
+        <div className="avatar">
+          <div className="w-40 rounded mr">
+            <img
+              src={"../../" + user.image}
+              alt="User Image"
+              className="w-full h-full object-cover"
+            />
+          </div>
         </div>
       </div>
-      
-      <Footer />
+
+      <div style={{ position: "fixed", bottom: "0", width: "100%" }}>
+        <Footer />
+      </div>
+      <dialog id="modal_ChangePass" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Change Password!</h3>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">New Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+              value={password}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Confirm New Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+              value={newPassword}
+            />
+          </div>
+          <div className="modal-action flex">
+            <form method="dialog">
+              <button className="btn btn-primary mr-2" onClick={handleChangePass}>Submit</button>
+            </form>
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
